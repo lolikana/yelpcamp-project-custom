b5e7eb0ef0ef01e7b2f037bc5eb78d10f2e85367
lolikana <thomas.guiart@gmail.com>
2022-10-25 17:44:19 +0900

    setup ejs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
 app.get('/', (_req, res) => {
-  res.send('HomePage');
+  res.render('index');
 });
 
 app.listen(3000, () => {
