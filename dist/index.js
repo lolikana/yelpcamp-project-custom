0e5e832bb2eb6e116799505e970bd966501b0658
lolikana <thomas.guiart@gmail.com>
2022-10-27 22:47:40 +0900

    campground index

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const ejs_mate_1 = __importDefault(require("ejs-mate"));
+const campgrounds_1 = require("./models/campgrounds");
 const app = (0, express_1.default)();
 mongoose_1.default.connect('mongodb://localhost:27017/yelp-camp');
 const db = mongoose_1.default.connection;
@@ -20,6 +21,10 @@ app.set('views', path_1.default.join(__dirname, '../views'));
 app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 app.get('/', (_req, res) => {
     res.render('index');
+});
+app.get('/campgrounds', async (_req, res) => {
+    const campgrounds = await campgrounds_1.CampgroundModel.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
 app.listen(3000, () => {
     console.log('Serving on port 3000');
