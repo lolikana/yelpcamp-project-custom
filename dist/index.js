9d657f57d219a758d3dae5d617d764f6fb7ab060
Lolikana <thomas.guiart@gmail.com>
2022-11-07 21:03:10 +0900

    added husky and lint-stagged

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ejs_mate_1 = __importDefault(require("ejs-mate"));
-const express_1 = __importDefault(require("express"));
 const method_override_1 = __importDefault(require("method-override"));
+const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const path_1 = __importDefault(require("path"));
 const campgrounds_1 = require("./models/campgrounds");
