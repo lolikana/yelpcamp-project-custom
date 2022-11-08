3cb1b0bd6690e7940299e78f8a6ac4e42113b282
Lolikana <91938540+lolikana@users.noreply.github.com>
2022-11-08 18:17:54 +0900

    Config husky (#1)
    * add views to src
    
    * added husky and lint-stagged
    
    * test lint-staged eslint fix
    
    * refactoring
    
    * fix husky and lint-staged
    
    * test after fix husky

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,29 +3,6 @@ const burgerTrigger = document.getElementById('burger__btn');
 // const navbarMenuList = document.getElementById('navbar__list');
 const navLink = document.querySelectorAll('[aria-current]');
 
-// START Create nav bar list
-// const navbarLink: string[][] = [
-//   ['Homepage', '/'],
-//   ['Campgrounds', '/campgrounds'],
-//   ['Add', '/campgrounds/new']
-// ];
-
-// // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-// navbarMenuList!.innerHTML = navbarLink
-//   .map(
-//     ([title, url]: string[]) =>
-//       `   <li>
-//       <a
-//         href=${url}
-//         class="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-//         aria-current="page"
-//       >
-//         ${title}
-//       </a>
-//       </li>`
-//   )
-// .join('');
-
 const url = document.URL.slice(21);
 console.log(url);
 console.log(navLink);
