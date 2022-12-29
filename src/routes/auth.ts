728f44dd14f9e0b8f77b4eae14f30b2242d480be
Lolikana <thomas.guiart@gmail.com>
2022-12-29 10:08:56 +0900

    login routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import passport from 'passport';
 
+import { User } from '../models/user';
 import { catchAsync } from '../utils/catchAsync';
 
 export const router = Router();
@@ -11,6 +13,32 @@ router.get('/register', (_req, res) => {
 router.post(
   '/register',
   catchAsync(async (req, res) => {
-    res.send(req.body);
+    try {
+      const { email, username, password } = req.body;
+      const user = await new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      console.log(registeredUser);
+      req.flash('success', 'Welcome to Yelp Camp');
+      res.redirect('/campgrounds');
+    } catch (err: any) {
+      req.flash('error', err.message);
+      res.redirect('/register');
+    }
   })
+);
+
+router.get('/login', (_req, res) => {
+  res.render('auth/login');
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login'
+  }),
+  (req, res) => {
+    req.flash('success', 'Welcome back');
+    res.redirect('/campgrounds');
+  }
 );
