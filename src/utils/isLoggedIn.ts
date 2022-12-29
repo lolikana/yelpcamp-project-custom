b4c05e80f24a44289b1bac2ec7647360c2f15de7
Lolikana <thomas.guiart@gmail.com>
2022-12-29 10:26:39 +0900

    loggedIn middleware

---
+++ src/utils/isLoggedIn.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+const isLoggedIn = (req: Request, res: Response, next: NextFunction): void => {
+  if (!req.isAuthenticated()) {
+    req.flash('error', 'You must be signed in');
+    res.redirect('/login');
+  }
+  next();
+};
+
+export default isLoggedIn;
