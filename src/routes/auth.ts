a5791b2c47a0db6bd0b50f15bf11dc07f236a499
Lolikana <thomas.guiart@gmail.com>
2023-01-04 15:35:52 +0900

    refactoring routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,20 +6,21 @@ import { catchAsync } from '../utils/catchAsync';
 
 export const router = Router();
 
-router.get('/register', auth.renderRegister);
+router
+  .route('/register')
+  .get(auth.renderRegister)
+  .post(catchAsync(auth.register));
 
-router.post('/register', catchAsync(auth.register));
-
-router.get('/login', auth.renderLogin);
-
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login',
-    keepSessionInfo: true
-  }),
-  auth.login
-);
+router
+  .route('/login')
+  .get(auth.renderLogin)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/login',
+      keepSessionInfo: true
+    }),
+    auth.login
+  );
 
 router.get('/logout', auth.logout);
