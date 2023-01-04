99ef7fdc6d07a5f7fd0160046553518347dfc6dd
Lolikana <thomas.guiart@gmail.com>
2023-01-04 15:25:48 +0900

    refactoring reviews and auth controllers

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,38 +1,16 @@
 import { Router } from 'express';
 import passport from 'passport';
 
-import { User } from '../models/user';
+import * as auth from '../controllers/auth';
 import { catchAsync } from '../utils/catchAsync';
 
 export const router = Router();
 
-router.get('/register', (_req, res) => {
-  res.render('auth/register');
-});
+router.get('/register', auth.renderRegister);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = await new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err: any) => {
-        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        if (err) return next(err);
-        req.flash('success', 'Welcome to Yelp Camp');
-        res.redirect('/campgrounds');
-      });
-    } catch (err: any) {
-      req.flash('error', err.message);
-      res.redirect('/register');
-    }
-  })
-);
+router.post('/register', catchAsync(auth.register));
 
-router.get('/login', (_req, res) => {
-  res.render('auth/login');
-});
+router.get('/login', auth.renderLogin);
 
 router.post(
   '/login',
@@ -41,19 +19,7 @@ router.post(
     failureRedirect: '/login',
     keepSessionInfo: true
   }),
-  (req, res) => {
-    req.flash('success', 'Welcome back');
-    const redirectUrl =
-      req.session.returnTo !== undefined
-        ? req.session.returnTo
-        : '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  auth.login
 );
 
-router.get('/logout', (req, res) => {
-  req.logout((err: unknown) => err !== undefined && console.log(err));
-  req.flash('success', 'logout');
-  res.redirect('/campgrounds');
-});
+router.get('/logout', auth.logout);
--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 
+import * as reviews from '../controllers/reviews';
 import { validateReview } from '../libs/validations';
-import { CampgroundModel } from '../models/campgrounds';
-import { ReviewModel } from '../models/review';
 import { catchAsync } from '../utils/catchAsync';
 import isLoggedIn from '../utils/isLoggedIn';
 import isReviewAuthor from '../utils/isReviewAuthor';
@@ -10,35 +9,12 @@ import isReviewAuthor from '../utils/isReviewAuthor';
 // Need to merge params to get campgrounds/:id
 export const router = Router({ mergeParams: true });
 
-router.post(
-  '/',
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await CampgroundModel.findById(id);
-    const review = new ReviewModel(req.body.review);
-    review.author = (req.user as any)._id;
-    campground?.reviews.push(review.id);
-    await campground?.save();
-    await review.save();
-    req.flash('success', 'Created new review');
-    res.redirect(`/campgrounds/${campground?._id}`);
-  })
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.create));
 
 router.delete(
   '/:reviewId',
   isLoggedIn,
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await CampgroundModel.findByIdAndUpdate(id, {
-      $pull: { reviews: reviewId }
-    });
-    await ReviewModel.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review');
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.destroy)
 );
