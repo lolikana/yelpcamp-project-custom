ed03323d02349cbdfad4c70dcb4f782e232db24e
Lolikana <thomas.guiart@gmail.com>
2023-01-04 15:35:52 +0900

    refactoring routes

--- src/routes/campgrounds.ts
+++ src/routes/campgrounds.ts
@@ -8,20 +8,30 @@ import isLoggedIn from '../utils/isLoggedIn';
 
 export const router = Router();
 
-router.get('/', catchAsync(campgrounds.index) as RequestHandler);
+router
+  .route('/')
+  .get(catchAsync(campgrounds.index) as RequestHandler)
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.create) as RequestHandler
+  );
 
 router.get('/new', isLoggedIn, (_req, res) => {
   res.render('campgrounds/new');
 });
 
-router.post(
-  '/',
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.create) as RequestHandler
-);
-
-router.get('/:id', catchAsync(campgrounds.read) as RequestHandler);
+router
+  .route('/:id')
+  .get(catchAsync(campgrounds.read) as RequestHandler)
+  .put(
+    isLoggedIn,
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.update) as RequestHandler
+  )
+  .delete(isLoggedIn, catchAsync(campgrounds.destroy) as RequestHandler);
 
 router.get(
   '/:id/edit',
@@ -29,19 +39,4 @@ router.get(
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   isAuthor,
   catchAsync(campgrounds.updateForm) as RequestHandler
-);
-
-router.put(
-  '/:id',
-  isLoggedIn,
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.update) as RequestHandler
-);
-
-router.delete(
-  '/:id',
-  isLoggedIn,
-  catchAsync(campgrounds.destroy) as RequestHandler
 );
