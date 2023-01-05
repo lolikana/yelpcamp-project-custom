df533bc143d7f4383a934fa61bc4aafcb6b3bfc3
Lolikana <thomas.guiart@gmail.com>
2023-01-05 14:15:50 +0900

    setup cloudinary, multer cloudinary config

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const connect_flash_1 = __importDefault(require("connect-flash"));
+const dotenv = __importStar(require("dotenv"));
 const ejs_mate_1 = __importDefault(require("ejs-mate"));
 const express_1 = __importDefault(require("express"));
 const express_session_1 = __importDefault(require("express-session"));
@@ -17,6 +41,9 @@ const auth_1 = require("./routes/auth");
 const campgrounds_1 = require("./routes/campgrounds");
 const reviews_1 = require("./routes/reviews");
 const utils_1 = require("./utils");
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 const app = (0, express_1.default)();
 mongoose_1.default
     .connect('mongodb://127.0.0.1:27017/yelp-camp')
