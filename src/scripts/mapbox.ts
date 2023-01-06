0a90573725fd1f2a947623b163be25c4e48f7d2e
Lolikana <thomas.guiart@gmail.com>
2023-01-06 16:38:46 +0900

    map pop and style

--- src/scripts/mapbox.ts
+++
@@ -1,8 +0,0 @@
-const map = new mapboxgl.Map({
-  container: 'mapbox', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
-});
-
-new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
