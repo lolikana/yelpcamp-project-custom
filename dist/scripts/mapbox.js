4fe21527bba8234b40879c5c8f65954ea2e35b78
Lolikana <thomas.guiart@gmail.com>
2023-01-06 15:54:57 +0900

    display map

---
+++ dist/scripts/mapbox.js
@@ -0,0 +1,9 @@
+"use strict";
+const map = new mapboxgl.Map({
+    container: 'mapbox',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-74.5, 40],
+    zoom: 9
+});
+console.log(map);
+//# sourceMappingURL=mapbox.js.map
---
+++ src/scripts/mapbox.ts
@@ -0,0 +1,7 @@
+const map = new mapboxgl.Map({
+  container: 'mapbox', // container ID
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+});
+console.log(map);
